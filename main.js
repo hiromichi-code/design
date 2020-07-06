68f85c3601cce107e64575d9c1364a70f771dbf8
hiromichi shinohara <hiromichishinohara@hiromichinoMacBook-Pro.local>
2020-07-07 05:45:53 +0900

    1

--- main.js
+++ main.js
@@ -1,6 +1,99 @@
 'use strict';
 
-function book-size(){
-  
-}
+const tgtDropdownMenu = document.querySelector("#book-size");
 
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "A4"){
+    document.getElementById('main1').textContent ="210";
+    document.getElementById('main2').textContent ="210";
+    document.getElementById('main3').textContent ="210";
+    document.getElementById('main4').textContent ="210";
+    document.getElementById('bottom-out1').textContent ="297";
+    document.getElementById('bottom-out2').textContent ="297";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "A5"){
+    document.getElementById('main1').textContent ="148";
+    document.getElementById('main2').textContent ="148";
+    document.getElementById('main3').textContent ="148";
+    document.getElementById('main4').textContent ="148";
+    document.getElementById('bottom-out1').textContent ="210";
+    document.getElementById('bottom-out2').textContent ="210";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "B5"){
+    document.getElementById('main1').textContent ="182";
+    document.getElementById('main2').textContent ="182";
+    document.getElementById('main3').textContent ="182";
+    document.getElementById('main4').textContent ="182";
+    document.getElementById('bottom-out1').textContent ="257";
+    document.getElementById('bottom-out2').textContent ="257";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "B6"){
+    document.getElementById('main1').textContent ="128";
+    document.getElementById('main2').textContent ="128";
+    document.getElementById('main3').textContent ="128";
+    document.getElementById('main4').textContent ="128";
+    document.getElementById('bottom-out1').textContent ="182";
+    document.getElementById('bottom-out2').textContent ="182";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "四六判"){
+    document.getElementById('main1').textContent ="128";
+    document.getElementById('main2').textContent ="128";
+    document.getElementById('main3').textContent ="128";
+    document.getElementById('main4').textContent ="128";
+    document.getElementById('bottom-out1').textContent ="188";
+    document.getElementById('bottom-out2').textContent ="188";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "A6"){
+    document.getElementById('main1').textContent ="105";
+    document.getElementById('main2').textContent ="105";
+    document.getElementById('main3').textContent ="105";
+    document.getElementById('main4').textContent ="105";
+    document.getElementById('bottom-out1').textContent ="148";
+    document.getElementById('bottom-out2').textContent ="148";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "B4"){
+    document.getElementById('main1').textContent ="384";
+    document.getElementById('main2').textContent ="384";
+    document.getElementById('main3').textContent ="384";
+    document.getElementById('main4').textContent ="384";
+    document.getElementById('bottom-out1').textContent ="257";
+    document.getElementById('bottom-out2').textContent ="257";
+  }
+});
+
+tgtDropdownMenu.addEventListener('change', (e)　=> {
+  let value = tgtDropdownMenu.value;
+  if(value === "A3"){
+    document.getElementById('main1').textContent ="420";
+    document.getElementById('main2').textContent ="420";
+    document.getElementById('main3').textContent ="420";
+    document.getElementById('main4').textContent ="420";
+    document.getElementById('bottom-out1').textContent ="297";
+    document.getElementById('bottom-out2').textContent ="297";
+  }
+});
